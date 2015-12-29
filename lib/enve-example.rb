6569b6563e522aba1ee130b80f2e378933d11462
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2015-12-29 13:44:14 -0500

    Minor assertion to ease debugging

--- lib/enve-example.rb
+++ lib/enve-example.rb
@@ -7,6 +7,8 @@ class EnveExample
       @description = description.join(" ") if description.is_a? Array
       @values ||= []
       @values = values.map{ |i| i.is_a?(Array) ? i : [i] }
+      raise "Task #{hash[:task]} couldn't be located in the collection." if
+	 task.nil?
       raise "The task #{task.task} has #{task.options.count} options, but "+
 	 "the example has #{values.count} values." unless
 	 values.count == task.options.count
